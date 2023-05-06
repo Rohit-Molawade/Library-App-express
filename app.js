05ee6b8806dd3c314d7a3b1088ade973567fc808
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-05-06 18:40:58 +0530

    First version of application moved into github

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+const mongoose = require('mongoose');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+let catalogRouter = require('./routes/catalog');
+
+var app = express();
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(compression()); // Compress all routes
+
+// Set up mongoose connection
+mongoose.set('strictQuery', false);
+const mongoDB = 'mongodb+srv://rohitmolawade27:Rohit2706@local-library.cmflrtb.mongodb.net/?retryWrites=true&w=majority';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
